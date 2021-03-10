48fb3634982196e15c5df68c7bf9f0ff390ca2c6
Caio Machado <caioamsul@gmail.com>
2021-03-10 18:58:14 -0300

    Correct variable declarations

--- app/src/main/java/com/example/salariodomes/MainActivity.kt
+++ app/src/main/java/com/example/salariodomes/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.salariodomes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var nomeFuncionario = txtNomeFuncionario.text.toString()
-        var valHoraTrabalhada = txtValorHoraTrabalhada.text.toString().toFloat()
-        var qtdDiasTrabalhados = txtQtDiasTrabalhados.text.toString().toInt()
-        var qtdHoraDia = txtQtdHorasDia.text.toString().toInt()
+        val textViewNomeFuncionario : TextInputEditText = findViewById(R.id.txtNomeFuncionario)
+        val textViewHoraTrabalhada : TextInputEditText = findViewById(R.id.txtValorHoraTrabalhada)
+        val textQtDiasTrabalhados : TextInputEditText = findViewById(R.id.txtQtDiasTrabalhados)
+        val textQtdHorasDia : TextInputEditText = findViewById(R.id.txtQtdHorasDia)
+
+
+        var nomeFuncionario = textViewNomeFuncionario.text.toString()
+        var valHoraTrabalhada = textViewHoraTrabalhada.text.toString().toFloat()
+        var qtdDiasTrabalhados = textQtDiasTrabalhados.text.toString().toInt()
+        var qtdHoraDia = textQtdHorasDia.text.toString().toInt()
 
             fun calculaSalario(){
                 
